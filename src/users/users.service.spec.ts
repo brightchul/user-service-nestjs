615b207021407d66c2f8e276c088a86ba792a9bc
brightchul <abhbtbb1@gmail.com>
2023-02-01 22:58:51 +0900

    feat: add user service test

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,45 +1,30 @@
+import { UnprocessableEntityException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
-import { getRepositoryToken } from '@nestjs/typeorm';
-import { DataSource, Repository } from 'typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { UserEntity } from './entities/user.entity';
+import { UsersModule } from './users.module';
 import { UsersService } from './users.service';
 
-type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
-
-const userEntityData = {
-  id: '1',
-  name: 'tom',
-  email: 'tom@email.com',
-  password: '!@#$',
-};
-
-const mockRepository = () => ({
-  findOne: jest.fn(),
-});
-
 describe('UsersService', () => {
+  let module: TestingModule;
   let service: UsersService;
-  let userRepository: MockRepository<UserEntity>;
-
-  const MockDataSource = {
-    provide: DataSource,
-    useClass: class {
-      transaction = jest.fn();
-    },
-  };
-
-  const MockUserRepository = {
-    provide: getRepositoryToken(UserEntity),
-    useFactory: mockRepository,
-  };
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService, MockDataSource, MockUserRepository],
+    module = await Test.createTestingModule({
+      imports: [
+        TypeOrmModule.forRoot({
+          type: 'sqlite',
+          database: ':memory:',
+          entities: [UserEntity],
+          synchronize: true,
+        }),
+        UsersModule,
+      ],
     }).compile();
 
     service = module.get(UsersService);
-    userRepository = module.get('UserEntityRepository');
+    await service.createUser('tom', 'tom@email.com', '!@#$');
   });
 
   it('should be defined', () => {
@@ -47,14 +32,27 @@ describe('UsersService', () => {
   });
 
   describe('hasEmail', () => {
-    it('result is true', async () => {
-      jest.spyOn(userRepository, 'findOne').mockResolvedValue(userEntityData);
+    it('should return true when email exists', async () => {
       expect(await service.hasEmail('tom@email.com')).toBeTruthy();
     });
 
-    it('result is null', async () => {
-      jest.spyOn(userRepository, 'findOne').mockResolvedValue(null);
+    it('should return false when email does not exist ', async () => {
       expect(await service.hasEmail('notExistedEmail@email.com')).toBeFalsy();
+    });
+  });
+
+  describe('createUser', () => {
+    it('should create new user when email is not existed', async () => {
+      expect(await service.hasEmail('to1m@email.com')).toBeFalsy();
+      await service.createUser('tom', 'to1m@email.com', '!@#$');
+      expect(await service.hasEmail('to1m@email.com')).toBeTruthy();
+    });
+
+    it('should throw Error when email is existed', async () => {
+      const result = service.createUser('tom', 'tom@email.com', '!@#$');
+      await expect(result).rejects.toThrowError(
+        new UnprocessableEntityException('이미 가입된 이메일 입니다.'),
+      );
     });
   });
 });

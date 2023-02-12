85f44045286e2ce93210b6524dfd03bb6ed64e2c
brightchul <abhbtbb1@gmail.com>
2023-02-12 11:55:19 +0900

    feat: add signupVerifyToken in user entity

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,6 @@
 import { DataSource, Repository } from 'typeorm';
 import { ulid } from 'ulid';
+import * as uuid from 'uuid';
 
 import { Injectable, UnprocessableEntityException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -19,12 +20,15 @@ export class UsersService {
       throw new UnprocessableEntityException('이미 가입된 이메일 입니다.');
     }
 
+    const signupVerifyToken = uuid.v1();
+
     await this.datasource.transaction(async (manager) => {
       const user = new UserEntity();
       user.id = ulid();
       user.name = name;
       user.email = email;
       user.password = password;
+      user.signupVerifyToken = signupVerifyToken;
 
       await manager.save(user);
     });
